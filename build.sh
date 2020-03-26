b1d8ba6a2c63f27943d22a0179dd4541f06f33f2
zhezhao <zhezhaoxu@gmail.com>
2020-03-26 23:08:53 +0800

    update

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 # build pkg
-bazel build -c opt --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-mfpmath=both --copt=-msse4.2 --jobs 3 --local_ram_resources=4096 //tensorflow/tools/pip_package:build_pip_package
+bazel build -c opt \
+    --copt=-mavx --copt=-mavx2 --copt=-mfma \
+    --copt=-mfpmath=both --copt=-msse4.2 \
+    --jobs 3 --local_ram_resources=4096 \
+    --define=tensorflow_mkldnn_contraction_kernel=0 \
+    //tensorflow/tools/pip_package:build_pip_package
