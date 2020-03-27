a25a659a59b66a8bf53b31fad2580e14f629e47c
zhezhao <zhezhaoxu@gmail.com>
2020-03-27 12:05:49 +0800

    update

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
-# build pkg
+# build tensorflow pip pkg
+# mkl need test
 bazel build -c opt \
-    --copt=-mavx --copt=-mavx2 --copt=-mfma \
-    --copt=-mfpmath=both --copt=-msse4.2 \
-    --jobs 3 --local_ram_resources=4096 \
+    --copt=-mavx \
+    --copt=-mavx2 \
+    --copt=-mfma \
+    --copt=-mfpmath=both \
+    --copt=-msse4.2 \
+    --jobs 3 \
+    --local_ram_resources=4096 \
     --define=tensorflow_mkldnn_contraction_kernel=0 \
     //tensorflow/tools/pip_package:build_pip_package
--- build_so.sh
+++ build_so.sh
@@ -1,2 +1,12 @@
-# build .so
-bazel build -c opt --copt=-mavx --copt=-mavx2 --copt=-mfma --copt=-mfpmath=both --copt=-msse4.2 --jobs 3  --local_ram_resources=4096 //tensorflow:libtensorflow_cc.so
+# build libtensorflow_cc.so
+# mkl need test
+bazel build -c opt \
+    --copt=-mavx \
+    --copt=-mavx2 \
+    --copt=-mfma \
+    --copt=-mfpmath=both \
+    --copt=-msse4.2 \
+    --jobs 3 \
+    --local_ram_resources=4096 \
+    --define=tensorflow_mkldnn_contraction_kernel=0 \
+    //tensorflow:libtensorflow_cc.so
