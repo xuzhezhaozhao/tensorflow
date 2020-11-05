fbccabacc6ee3a949a1f900e5fc9b02278288bc6
徐哲钊 <11118261@vivo.com>
2020-11-05 10:16:49 +0800

    update make_dist.sh

--- make_dist.sh
+++ make_dist.sh
@@ -2,5 +2,6 @@ mkdir -p dist
 mkdir -p dist/lib
 mkdir -p dist/include
 
-cp -r bazel-genfiles/tensorflow/libtensorflow_cc* dist/lib
+cp -Lf bazel-bin/tensorflow/libtensorflow_cc.so dist/lib
+cp -Lf bazel-bin/tensorflow/libtensorflow_framework.so dist/lib
 find tensorflow/ -name '*.h' | xargs -i sh -c 'mkdir -p dist/include/`dirname {}`; cp {} dist/include/`dirname {}`'
